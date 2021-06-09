69dc693855f797cf1c32fd1e4b198d3271e31cc0
AllisonWonderland <aneary91@gmail.com>
2021-06-09 15:10:03 -0500

    added more tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,46 +1,65 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   Tag.findAll({
-    include: [
-{      model: Product, {
-  model: Tag,
-  through: ProductTag
-}
-    ]
+    include: [{ model: Product, through: ProductTag }],
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => res.json(err));
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   Tag.findOne({
     where: {
       id: req.params.id,
     },
     include: [
-      Product,{
+      Product,
+      {
         model: Tag,
         through: ProductTag,
-      }
-    ]
+      },
+    ],
   })
+    .then((tag) => res.json(tag))
+    .catch((err) => res.json(err));
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
+  tag
+    .create(req.body)
+    .then((tag) => res.json(tag))
+    .catch((err) => res.json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body{
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((tag) => res.json(tag))
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((tag) => res.json(tag))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
